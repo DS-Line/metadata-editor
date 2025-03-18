25efc26e81144a5f9f23b173ff013e01c47075cc
Ritesh048 <ritesh.pandey@lisnepal.com.np>
2025-03-18 19:53:19 +0545

    chore(added-dialog): added dialog for delete meta

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../ui/utils"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { buttonVariants } from "@/components/ui/button"
