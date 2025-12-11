0bed15124190864712c0bd44a2397f0e66b9dbca
Ritesh048 <ritesh.pandey@yco.com.np>
2025-12-11 09:49:30 +0545

    fix(linter): added linter fixes

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,7 +1,8 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "./utils"
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -24,7 +25,8 @@ const badgeVariants = cva(
 )
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
+  extends
+    React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
