42dea8b4ace32a45362e4e4581111b370884665c
Ritesh048 <ritesh.pandey@yco.com.np>
2026-01-03 17:29:30 +0545

    style(upgrade): Upgraded and Migrated Tailwind To Latest

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
 import { ButtonProps, buttonVariants } from "@/components/ui/button"
+
+import { cn } from "./utils"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
